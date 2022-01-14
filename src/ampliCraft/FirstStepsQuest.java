15eb4d4dbd043cd91efd11a755d65637db4cd10f
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2022-01-14 02:52:10 +0100

    3

--- src/ampliCraft/FirstStepsQuest.java
+++ src/ampliCraft/FirstStepsQuest.java
@@ -15,9 +15,10 @@ public class FirstStepsQuest extends StelaritQuest {
 		this.targetMob = EntityType.SPIDER;
 		this.lastStoryProgress = 2;
 		this.targetMobNumber = 3;
-		this.questStartText = ChatColor.BLUE + "Du siehst so aus als würdest du Arbeit suchen? Wir haben vor der Taverne immer wieder Probeme mit Spinnen. " + ChatColor.GOLD + "Töte 3 davon und du erhälst eine Belohnung!";
+		this.questStartText = ChatColor.BLUE + "Du siehst so aus als würdest du Arbeit suchen? Wir haben vor der Taverne immer wieder Probeme mit Spinnen. " + ChatColor.GOLD + "Töte 3 davon und du erhälst eine Belohnung" + ChatColor.BLUE + "!";
 		this.questLastText = "Danke! Endlich muss ich mir keine sorgen mehr machen, dass die Hälfte meiner Kundschaft auf dem Weg zu meiner Kneipe aufgefressen wird. Falls du wieder Arbeit suchst sprich mich an. Achso hier ist noch ein bisschen Geld für dich!";
 		this.rewardMoney = 50;
+		this.type = QuestType.KILLQUEST;
 	}
 
 	@Override
